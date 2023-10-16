422c6741f29a0eda3d8b99402232b76be6c6520f
Tiago Barbosa <up202004926@edu.fe.up.pt>
2023-10-16 15:39:26 +0100

    fixed cake

--- tp1/MyCake.js
+++ tp1/MyCake.js
@@ -16,7 +16,34 @@ export class MyCake {
         const cake = new THREE.CylinderGeometry(this.radius, this.radius, this.height, 32, 1, false, 0, Math.PI * 2 * 0.90);
         let cakeMesh = new THREE.Mesh( cake, this.cakeMaterial );
         cakeMesh.position.copy(this.position); 
-        this.app.scene.add(cakeMesh);
+
+        // plane to cover the cut of the cylinder
+        let plane = new THREE.PlaneGeometry(this.height,this.radius);
+        let planeMesh = new THREE.Mesh(plane, this.cakeMaterial);
+        planeMesh.position.set(this.position.x, this.position.y , this.position.z + this.radius/2);
+        planeMesh.rotation.z = (Math.PI / 2);
+        planeMesh.rotation.y = (Math.PI / 2);
+
+        // plane to cover the cut of the cylinder
+        let plane2 = new THREE.PlaneGeometry(this.height,this.radius);
+        let planeMesh2 = new THREE.Mesh(plane, this.cakeMaterial);
+        planeMesh2.position.set(this.position.x, this.position.y , this.position.z);
+        planeMesh2.rotation.z = (Math.PI / 2);
+        planeMesh2.rotation.y = (Math.PI / 2);
+        // rotation in relation to the center
+        planeMesh2.rotateX(-Math.PI * 2 *0.10);
+        // adjust the position
+        planeMesh2.position.set(this.position.x - 0.185, this.position.y, this.position.z + this.radius/2 -0.06);
+
+
+        // group
+
+        let group = new THREE.Group();
+        group.add(cakeMesh);
+        group.add(planeMesh);
+        group.add(planeMesh2);
+
+        this.app.scene.add(group);
   }
 
 }
