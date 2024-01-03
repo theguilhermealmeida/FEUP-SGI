c6fa795d2e8a0aaa1ff33fde80e9827bea3674c8
Tiago Barbosa <up202004926@edu.fe.up.pt>
2024-01-03 21:45:59 +0000

    final commit

--- tp3/MyContents.js
+++ tp3/MyContents.js
@@ -39,7 +39,7 @@ class MyContents  {
         if (this.axis === null) {
             // create and attach the axis to the scene
             this.axis = new MyAxis(this)
-            this.app.scene.add(this.axis)
+            //this.app.scene.add(this.axis)
         }
     }
 
